6c37d5484e9b8418d09a1ab370e0a462265a1495
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-09-14 14:30:47 +0300

    Homepage Working

--- assets/src/script/custom/app.js
+++ assets/src/script/custom/app.js
@@ -22,3 +22,45 @@ $('.headsliderowl').owlCarousel({
     }
 });
 /* Head Home Slider */
+
+/* Research News Category List Owl Carousel */
+$('.researchnewscatlistowl').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    items:4,
+    autoWidth:false,
+    mouseDrag:true,
+    animateIn: 'fadeInLeft',
+    animateOut: 'fadeOutRight',
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            margin:0
+        },
+        819:{
+            items:2,
+            margin:20
+        },
+        1059:{
+            items:3,
+            margin:20
+        },
+        1379:{
+            items:3,
+            margin:25
+        },
+        1619:{
+            items:4,
+            margin:30
+        },
+        1939:{
+            margin:30
+        }
+    }
+});
+/* Research News Category List Owl Carousel */
